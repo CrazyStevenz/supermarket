3951027bff931d9d21b624f8e5268c69c07dfb16
CrazyStevenz <spapanatsios@gmail.com>
2019-05-20 17:54:07 +0300

    Added Product Model - Removed cart - Added details box - Fixed #1

--- src/supermarket/ProductController.java
+++ src/supermarket/ProductController.java
@@ -3,13 +3,12 @@ package supermarket;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
 import org.apache.commons.dbutils.DbUtils;
 import java.sql.*;
 
-public class ProductController
-{
+public class ProductController {
     private static String driverClassName = "org.postgresql.Driver";
     private static String url = GlobalConstants.DB_URL;
     private static String dbUsername = GlobalConstants.DB_USERNAME;
@@ -18,61 +17,54 @@ public class ProductController
     private static Statement st = null;
     private static PreparedStatement ps = null;
     private static ResultSet rs = null;
+    private static Product[] products = new Product[50];
 
-    private static int[] id = new int[50];
-    private static String[] name = new String[50];
-    private static float[] price = new float[50];
-    private static int[] stock = new int[50];
-
-    ListView<String> productList = new ListView<>();
-    ListView<String> cartList = new ListView<>();
-    ObservableList<String> productItems = FXCollections.observableArrayList();
-    ObservableList<String> cartItems = FXCollections.observableArrayList();
-
-    @FXML ScrollPane productScrollPane;
-    @FXML ScrollPane cartScrollPane;
+    @FXML ListView<String> productListView;
+    @FXML Label detailsLabel;
 
     @FXML
-    private void loadProducts()
-    {
-        try
-        {
+    private void loadProducts() {
+        try {
             Class.forName(driverClassName);
             conn = DriverManager.getConnection(url, dbUsername, dbPassword);
 
             String getProductsQuery =
-                "SELECT *" +
-                "FROM products";
+                    "SELECT id, name, price, stock " +
+                    "FROM products";
 
             st = conn.createStatement();
             rs = st.executeQuery(getProductsQuery);
 
-            int i = 0;
-            while (rs.next())
-            {
-                id[i] = rs.getInt("id");
-                name[i] = rs.getString("name");
-                price[i] = rs.getFloat("price");
-                stock[i] = rs.getInt("stock");
+            int id, stock, i = 0;
+            float price;
+            String name;
+
+            while (rs.next()) {
+                id = rs.getInt("id");
+                name = rs.getString("name");
+                price = rs.getFloat("price");
+                stock = rs.getInt("stock");
+
+                if (rs.wasNull()) {
+                    products[i] = new Product(id, name, price);
+                } else {
+                    products[i] = new Product(id, name, price, stock);
+                }
 
                 i++;
             }
 
-            for (int j = 0; j < i; j++)
-            {
-                productItems.add(name[j] + " - " + price[j] + "€ - " + stock[j] + " left");
+            ObservableList<String> productItems = FXCollections.observableArrayList();
+
+            for (int j = 0; j < i; j++) {
+                productItems.add(products[j].getName() + " - " + products[j].getPrice() + "€ - " + products[j].getStock());
             }
 
-            productList.setItems(productItems);
-            productList.setPrefWidth(325);
-            productScrollPane.setContent(productList);
-        }
-        catch (Exception e)
-        {
-            // TODO
-        }
-        finally
-        {
+            productListView.setItems(productItems);
+            productListView.setPrefWidth(325);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
             DbUtils.closeQuietly(rs);
             DbUtils.closeQuietly(ps);
             DbUtils.closeQuietly(conn);
@@ -80,11 +72,13 @@ public class ProductController
     }
 
     @FXML
-    private void moveToCart()
-    {
-        cartItems.add(productList.getSelectionModel().getSelectedItems().toString());
-        cartList.setItems(cartItems);
-        cartList.setPrefWidth(325);
-        cartScrollPane.setContent(cartList);
+    private void updateDetails() {
+        int index = productListView.getSelectionModel().getSelectedIndex();
+
+        detailsLabel.setText(
+                "Product Name: " + products[index].getName() +
+                 "\n\nPrice: " + products[index].getPrice() +
+                 "\n\nStock: " + products[index].getStock()
+        );
     }
 }
