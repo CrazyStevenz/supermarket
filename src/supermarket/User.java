b55169935940a6f15a31edb299bec2d58dd035e7
CrazyStevenz <stefan0spapanatsi0s@gmail.com>
2019-05-20 12:02:37 +0300

    Added User model

---
+++ src/supermarket/User.java
@@ -0,0 +1,47 @@
+package supermarket;
+
+class User {
+    private int id;
+    private String username;
+    private String name;
+    private int kind;
+
+    public User(int id, String username, String name, int kind) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.kind = kind;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getKind() {
+        return kind;
+    }
+
+    public void setKind(int kind) {
+        this.kind = kind;
+    }
+}
