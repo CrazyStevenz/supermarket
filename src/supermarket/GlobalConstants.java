4ba25c9cd77a279c85ff6902c09d335936d2938e
CrazyStevenz <spapanatsios@gmail.com>
2019-05-28 15:32:07 +0300

    Made GlobalConstants public

--- src/supermarket/GlobalConstants.java
+++ src/supermarket/GlobalConstants.java
@@ -2,9 +2,9 @@ package supermarket;
 
 public class GlobalConstants
 {
-    static final String DB_URL = "";
-    static final String DB_USERNAME = "";
-    static final String DB_PASSWORD = "";
+    public static final String DB_URL = "";
+    public static final String DB_USERNAME = "";
+    public static final String DB_PASSWORD = "";
 
     public static final int SCENE_WIDTH = 1024;
     public static final int SCENE_HEIGHT = 576;
